fb10038becb06700f4e2a61a06c961c76d45c1d2
InkVizitor32 <morgunov.roman89@gmail.com>
2022-11-07 23:00:51 +0300

    решили задачку через функции

--- Program.cs
+++ Program.cs
@@ -80,7 +80,7 @@ Console.WriteLine(" Введите число ");
 int x=Convert.ToInt32( Console.ReadLine());
 int count = 1;
 
-while (count <= x)
+while (count < x)
 {
 if  (count%2 == 0)   
 Console.Write( count+ " " );  
